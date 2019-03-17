3d739c6172acd9a044a2ea265d12a016942c13c1
Peter Hencz <peter.hencz@gmail.com>
2019-03-17 02:30:00 +0100

    play some card method - need refact

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,21 +6,29 @@ class Card extends Component {
     margin: '1em',
     bg: '#fff',
   };
+
   bogyoFullSize = () => {
     this.setState({
       bg: this.props.color,
     });
   };
 
+  plusSize = () => {
+    this.setState({
+      margin: '10em',
+    });
+  };
+
   render() {
     return (
-      <div className="card" style={{ background: this.state.bg }}>
+      <div className="card" style={{ background: this.state.bg, margin: this.state.margin }}>
         <div
           className="item-hexcode"
           onClick={this.bogyoFullSize}
           style={{ background: this.props.color }}
         />
         <h1>{this.props.color}</h1>
+        <p onClick={this.plusSize}>+</p>
       </div>
     );
   }
